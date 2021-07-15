e0a53feb6718faa7e9694361b1f935e6126654d9
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-07-15 08:55:11 -0300

    Create simulandoarray.js

---
+++ Javascript/arrays/simulandoarray.js
@@ -0,0 +1,22 @@
+const quaseArray = {
+    0: 'Rafael',
+    1: 'Ana',
+    2: 'bia'
+
+}
+
+console.log(quaseArray)
+
+Object.defineProperty(quaseArray, 'toString', {
+    value: function(){
+        return Object.values(this)
+    }, enumerable: false
+})
+
+
+console.log(quaseArray[0])
+
+
+const meuArray = ['rafael', 'ana', 'bia']
+
+console.log(quaseArrya.toString() )
