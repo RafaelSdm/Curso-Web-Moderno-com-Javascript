ef99064f53aa77f4bf4965beb3692b6aab490042
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-07-16 08:39:35 -0300

    Create map1.js

---
+++ Javascript/arrays/map1.js
@@ -0,0 +1,18 @@
+const nums = [1,2,3,4,5]
+
+// for com proposito
+
+let resultado = nums.map(function(e){
+    return e * 2
+})
+
+console.log(resultado)
+
+const soma = e => e + 10
+const triplo = e => e * 10
+const dinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
+
+
+resultado = nums.map(soma).map(triplo).map(dinheiro)
+
+console.log(resultado)
