10f108c5447b876fecb013a6011f42b04c4e65ca
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-07-29 21:56:14 -0300

    commit full course

---
+++ node/modulecliente.js
@@ -0,0 +1,12 @@
+const moduloa = require('./modulo')
+const modulob = require('./modulob')
+
+console.log(moduloa.ola)
+console.log(moduloa.bemVindo)
+console.log(moduloa.ateLogo)
+console.log(moduloa)
+
+
+console.log(modulob.bomDia)
+console.log(modulob.boaNoite())
+console.log(modulob)
---
+++ node/modulo.js
@@ -0,0 +1,3 @@
+this.ola = 'fala pesssoal'
+exports.bemVindo = "bem vindo ao node!"
+module.exports.ateLogo = "ate o proximo exemplo"
